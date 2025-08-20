2a56ba635127c94a4a18724f09116762f2db66bd
m9k3s <126334796+m9k3s@users.noreply.github.com>
2025-08-20 10:33:27 +0800

    add the css file and add 0,000,00

--- script/script.js
+++ script/script.js
@@ -7,8 +7,26 @@ function random_4d_number()
     r = Math.random()*10000
     r = Math.floor(r)
 
+    if(r<10)
+    {
+        r = "000" + String(r)
+    }
+    else if (r < 100)
+    {
+        r = "00" + String(r)
+    }
+    else if (r < 1000)
+    {
+        r = "0" + String(r)
+    }
+    else 
+    {
+        r = String(r)
+    }
+
     return r
 }
+
 
 for(let a= 0;a < 100 ;a++)
 {
